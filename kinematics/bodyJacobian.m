e5a177c5d5ccccb9ed46c5061a562642001d00b3
Dale McConachie <dmcconachie@wpi.edu>
2016-02-09 21:01:41 -0500

    Rename to remove implication that the transform to use is a specific one. Can be any frame rigidly connected to last link.

--- kinematics/bodyJacobian.m
+++ kinematics/bodyJacobian.m
@@ -1,5 +1,5 @@
-function J_b = bodyJacobian(twists, theta, g_theta)
+function J_b = bodyJacobian(twists, theta, g_current)
     J_s = spatialJacobian(twists, theta);
     
-    J_b = adj(g_theta) \ J_s;
+    J_b = adj(g_current) \ J_s;
 end
